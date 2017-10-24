98bc9e39d19cfdcd50ac86b79ffaad71e05cb695
tanhi <herrerart19@outlook.com>
2017-10-24 14:41:29 +0000

    modificando app.js

---
+++ README.md
@@ -0,0 +1,11 @@
+###Pseudocódigo
+
+1.Crear una función que reciba dos arreglos
+2.Comprobar si los dos arreglos tiene la misma longitud
+   2.1 Si no son iguales, el programa debe regresar false.
+   2.2 Si son iguales, va a continuar con el proceso.
+3.Comprobar uno a uno, los valores
+   3.1 Si no son iguales todos regresan false.
+   3.2 Si son exactamente iguales, continuar con el proceso.
+4.Si hemos llegado a este punto, regresar verdadero.
+5.FIN
--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var comparar = function(arreglo1,arreglo2){
   if (arreglo1.lenght === arreglo2.lenght) {
-    document.write("Si son iguales");
+    return false;
+  }else {
+    for (var i = 0; i < arreglo1.length; i+=1) {
+     if(arreglo1[i] === arreglo2[i]) {
+       return false;
+     }
+    }
+    document.write("Todas las condiciones se cumplen");
+    return true;
+   }
 
-  }
 }
+
 var array1 = [1,2,3,4,5,6,7,8,9,10];
 var array2 = [1,2,3,4,5,6,7,8,9,10];
 
