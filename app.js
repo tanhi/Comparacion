f917eaa1883dc044da8197f279366463a1c12b03
tanhi <herrerart19@outlook.com>
2017-10-24 14:12:04 +0000

    Subiendo mi primer Readme

---
+++ app.js
@@ -0,0 +1,10 @@
+var comparar = function(arreglo1,arreglo2){
+  if (arreglo1.lenght === arreglo2.lenght) {
+    document.write("Si son iguales");
+
+  }
+}
+var array1 = [1,2,3,4,5,6,7,8,9,10];
+var array2 = [1,2,3,4,5,6,7,8,9,10];
+
+comparar(array1,array2);
